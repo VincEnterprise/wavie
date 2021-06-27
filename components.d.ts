a02588d97bb52f8bee960306b370be25500ee344
Vincent van der Palen <hello@vincenterprise.com>
2021-06-27 15:30:13 +0200

    updates translations

--- components.d.ts
+++ components.d.ts
@@ -6,7 +6,6 @@ declare module 'vue' {
     Footer: typeof import('./src/components/Footer.vue')['default']
     Header: typeof import('./src/components/Header.vue')['default']
     DropdownButton: typeof import('./src/components/atoms/DropdownButton.vue')['default']
-    DropdownContent: typeof import('./src/components/atoms/DropdownContent.vue')['default']
     DropdownOption: typeof import('./src/components/atoms/DropdownOption.vue')['default']
     DropdownOptions: typeof import('./src/components/atoms/DropdownOptions.vue')['default']
     GlassPanel: typeof import('./src/components/atoms/GlassPanel.vue')['default']
@@ -15,7 +14,6 @@ declare module 'vue' {
     Toggle: typeof import('./src/components/atoms/Toggle.vue')['default']
     Dropdown: typeof import('./src/components/molecules/Dropdown.vue')['default']
     ConverterGroup: typeof import('./src/components/organisms/ConverterGroup.vue')['default']
-    CarbonLanguage: typeof import('virtual:vite-icons/carbon/language')['default']
   }
 }
 
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,8 +10,14 @@
           focus-visible:opacity-100
         "
   >
-    © {{ new Date().getFullYear() }} — Built by
+    © {{ new Date().getFullYear() }} — {{ t('built-by') }}
     {{ " " }}
     <a href="https://vincenterprise.com">VincEnterprise</a>
   </footer>
 </template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
