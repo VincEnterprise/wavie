03aedef09132e7c86cfa696ea6898e5bf7e3f79f
Vincent van der Palen <hello@vincenterprise.com>
2021-07-01 22:30:04 +0200

    fixes ios button :active

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,9 @@ export default defineConfig({
       'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
     },
   },
+  server: {
+    host: '0.0.0.0'
+  },
   plugins: [
     Vue({
       include: [/\.vue$/],
